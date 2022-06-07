6b81daff24f88255931a02424912a2712a0d51d7
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-06-07 19:13:28 -0300

    Configuracao, roteamento e loggin filter.

--- src/main/java/br/com/micro/apigateway/Application.java
+++ src/main/java/br/com/micro/apigateway/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApigatewayApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApigatewayApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
